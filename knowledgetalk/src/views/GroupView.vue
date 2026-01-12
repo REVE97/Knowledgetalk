21b1dad61f43e58e1cbb18a646374103724da756
REVE97 <zxcv9675@naver.com>
2026-01-12 11:25:08 +0900

    Feat: 주석 수정

--- knowledgetalk/src/views/GroupView.vue
+++ knowledgetalk/src/views/GroupView.vue
@@ -82,7 +82,7 @@ const cpCode = "KP-20200101-01";
 const authKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoi64Kg66as7KeA7Y-s7J247Yq4IiwibWF4VXNlciI6IjUwMDAwMDAwIiwic3RhcnREYXRlIjoiMjAyMC0wMS0wMVQwNjo0NzowMC4wMDBaIiwiZW5kRGF0ZSI6IjIwMzAtMTItMzFUMDY6NDc6MDAuMDAwWiIsImF1dGhDb2RlIjoiS1AtMjAyMDAxMDEtMDEiLCJjb21wYW55Q29kZSI6IkxJQy0wMSIsImlhdCI6MTU4NzUzODExNH0.73A0UiiMHJeIS8pIgoN4DfEWT4QCsMnXkO4uUdnfbYI";
 
-// 변수 상태관리
+// 상수 상태관리
 const roomId = ref("");
 const ready = ref(false);
 const joined = ref(false);
